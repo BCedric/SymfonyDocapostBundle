18421966eed2a0d75de9ce49029543805b1cd923
Cedbois <cedric.bois@uca.fr>
2024-06-19 16:19:28 +0200

    try config bundle

--- src/BCedricDocapostBundle.php
+++ src/BCedricDocapostBundle.php
@@ -11,20 +11,26 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class BCedricDocapostBundle extends Bundle
 {
 
-    // public function configure(DefinitionConfigurator $definition): void
-    // {
-    //     $definition->rootNode()
-    //         ->children()
-    //         ->scalarNode('pem_file')->end()
-    //         ->scalarNode('pem_password')->end()
-    //         ->scalarNode('url')->end()
-    //         ->scalarNode('siren')->end()
-    //         ->scalarNode('circuitId')->end()
-    //         ->end();
-    // }
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+            ->scalarNode('pem_file')->end()
+            ->scalarNode('pem_password')->end()
+            ->scalarNode('url')->end()
+            ->scalarNode('siren')->end()
+            ->scalarNode('circuitId')->end()
+            ->end();
+    }
 
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        $container->extension(BCedricDocapostExtension::class, $config);
+        $container->services()
+            ->get('b_cedric_docapost.docapost_fast')
+            ->arg('pem_file', $config['pem_file'])
+            ->arg('pem_password', $config['pem_password'])
+            ->arg('url', $config['url'])
+            ->arg('siren', $config['siren'])
+            ->arg('circuitId', $config['circuitId']);
     }
 }
