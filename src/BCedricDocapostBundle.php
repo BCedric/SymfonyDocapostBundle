0c267fee1e6e377e9ce01a7243782102bc6e3a18
Cedbois <cedric.bois@uca.fr>
2025-01-29 15:54:33 +0100

    set services in config

--- src/BCedricDocapostBundle.php
+++ src/BCedricDocapostBundle.php
@@ -2,15 +2,10 @@
 
 namespace BCedric\DocapostBundle;
 
-use BCedric\DocapostBundle\Command\SyncDocapostUsersCommand;
-use BCedric\DocapostBundle\Repository\DocapostUserRepository;
 use BCedric\DocapostBundle\Service\DocapostFast;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class BCedricDocapostBundle extends AbstractBundle
@@ -30,18 +25,7 @@ class BCedricDocapostBundle extends AbstractBundle
 
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        $builder->register(SyncDocapostUsersCommand::class)
-            ->setClass(SyncDocapostUsersCommand::class)
-            ->addTag('console.command')
-            ->addArgument(new Reference(DocapostFast::class))
-            ->addArgument(new Reference(DocapostUserRepository::class))
-            ->addArgument(new Reference(EntityManagerInterface::class))
-        ;
-
-        $builder->register(DocapostUserRepository::class)
-            ->setClass(DocapostUserRepository::class)
-            ->addArgument(new Reference(ManagerRegistry::class))
-        ;
+        $container->import('../config/services.yaml');
 
         $container->services()->set(DocapostFast::class)
             ->public();
@@ -53,5 +37,4 @@ class BCedricDocapostBundle extends AbstractBundle
             ->setArgument('$circuitId', $config['circuitId'])
             ->setArgument('$archives_dir', $config['archives_dir']);
     }
-
 }
